fbc79da4feb148847c733706d6867c7e647315d4
liviacristina74 <livia.c.alexandre@aluno.senai.br>
2025-08-21 15:58:50 -0300

    alterações do dia 21/08

---
+++ while.php
@@ -0,0 +1,18 @@
+<?php
+    $contador = 1;
+    while ($contador <= 5) {
+        echo "Numero:$contador <br>";
+        $contador++;
+    }
+
+?>
+<h3> Este é um while </h3>
+
+<?php    
+$contador = 1;
+    do {
+        echo "numero: $contador <br>";
+        $contador++;
+    } while (contador <= 5);
+
+?>
